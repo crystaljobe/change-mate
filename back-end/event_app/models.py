e42faf30f6c4a90c7e3a68170781a1be8d817f69
Kara Watts <kdwwatts@gmail.com>
2024-04-23 14:11:15 -0400

    updated event model and serializers to include start-end date and time and timezone and images

--- back-end/event_app/models.py
+++ back-end/event_app/models.py
@@ -3,48 +3,35 @@ from .validators import validate_event_type
 from profile_app.models import UserProfile
 from interest_app.models import InterestCategory
 
+
 # Create your models here.
 class Event(models.Model):
-    title = models.CharField(
-        max_length=50
-        )
-    date = models.DateField()
-    event_type = models.CharField(
-        validators=[validate_event_type]
-        )
+    title = models.CharField(max_length=50)
+    event_end = models.DateTimeField()
+    event_start = models.DateTimeField()
+    event_type = models.CharField(validators=[validate_event_type])
     virtual_event_link = models.URLField(
-        blank=True, 
+        blank=True,
         unique=True,
         null=True,
-        )
+    )
     event_venue = models.CharField(
         blank=True,
-        null=True, 
-        )
+        null=True,
+    )
     event_venue_address = models.CharField(
         blank=True,
         null=True,
-        )
-    time = models.CharField()
+    )
     time_zone = models.CharField()
     description = models.TextField()
-    event_photo = models.ImageField(
-        blank=True,
-        null=True,
-        upload_to = 'images/'
-        )
+    event_photo = models.TextField(null=True, blank=True)
     category = models.ForeignKey(
         InterestCategory,
         on_delete=models.CASCADE,
         related_name="events",
-        )
-    collaborators = models.ManyToManyField(
-        UserProfile,
-        related_name='user_events'
-        )
+    )
+    collaborators = models.ManyToManyField(UserProfile, related_name="user_events")
     users_attending = models.ManyToManyField(
-        UserProfile,
-        related_name='events_attending'
-        )
-
- 
+        UserProfile, related_name="events_attending"
+    )
