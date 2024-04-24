8cb39b1ab9803a650b5dc149d06709f888b034f4
LouisMartin07 <louis.martin11730@gmail.com>
2024-04-23 21:30:33 -0500

    updated utilites to have a event_photo and integrated it into editing event details

--- front-end/src/utilities/EventUtilities.jsx
+++ front-end/src/utilities/EventUtilities.jsx
@@ -39,7 +39,7 @@ export const setUserAttending = async (eventID, usersAttending) => {
     }
 };
 
-export const updateEventDetails = async (eventID, title, eventStart, eventEnd, timeZone, eventType, eventVenue, eventVenueAddress, description, category, virtualEventLink) => {
+export const updateEventDetails = async (eventID, title, eventStart, eventEnd, timeZone, eventType, eventVenue, eventVenueAddress, description, category, eventPhoto, virtualEventLink) => {
     let response = await api.put(`events/${eventID}/`, {
         "title" : title,
         "event_start" : eventStart,
@@ -50,6 +50,7 @@ export const updateEventDetails = async (eventID, title, eventStart, eventEnd, t
         "event_venue_address" : eventVenueAddress,
         "description" : description,
         "category" : category,
+        "event_photo" : eventPhoto,
 		"virtual_event_link": virtualEventLink
     });
     console.log(response.status);
