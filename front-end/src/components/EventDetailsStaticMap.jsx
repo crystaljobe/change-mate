2a3bdd0073dbe2e2e730177f56ee2ab60e10483d
crystaljobe <crystaljobe29@gmail.com>
2024-04-25 14:37:40 -0600

    saving update

--- front-end/src/components/EventDetailsStaticMap.jsx
+++ front-end/src/components/EventDetailsStaticMap.jsx
@@ -17,11 +17,18 @@ export default function LocationSearchMap() {
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
-          container: mapContainer.current,
-          style: 'mapbox://styles/mapbox/streets-v12',
-          center: [lng, lat],
-          zoom: zoom
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
         });
+
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+
       }, []);
 
     return (
