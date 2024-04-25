c011ba3df03d57993ebf1857cb60ac3388887ac1
ReginaFera13 <shawnmariesmith@icloud.com>
2024-04-25 17:39:32 -0400

    Adjusted search bar to be search form; Adjusted search logic to handle one or many search parameters; Created logic for rendering events where volunteers are needed

--- front-end/src/utilities/EventUtilities.jsx
+++ front-end/src/utilities/EventUtilities.jsx
@@ -6,34 +6,134 @@ export const getEventDetails = async (eventID) => {
     return eventDetails;
 };
 
-export const getEventDetailsByCategory = async (searchTerm) => {
-    const response = await api.get(`events/?category=${searchTerm}`);
+export const getEventDetailsAllFilters = async (searchLocation, searchDateStart, searchDateEnd, searchEventType, searchType, searchTerm) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}&end_date=${searchDateEnd}&type=${searchEventType}&${searchType}=${searchTerm}`);
     let eventDetails = response.data;
     return eventDetails;
 };
 
-export const getEventDetailsByType = async (searchTerm) => {
-    const response = await api.get(`events/?type=${searchTerm}`);
+export const getEventDetailsNoEventType = async (searchLocation, searchDateStart, searchDateEnd, searchType, searchTerm) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}&end_date=${searchDateEnd}&${searchType}=${searchTerm}`);
     let eventDetails = response.data;
     return eventDetails;
 };
 
-export const getEventDetailsByDate = async (searchDateStart, searchDateEnd) => {
-    if (searchDateStart && searchDateStart.length > 0 && searchDateEnd && searchDateEnd.length > 0) {
-        console.log('search date range')
-        const response = await api.get(`events/?start_date=${searchDateStart}&end_date=${searchDateEnd}`);
-        let eventDetails = response.data;
-        return eventDetails;
-    } else {
-        console.log('search exact date')
-        const response = await api.get(`events/?start_date=${searchDateStart}`);
-        let eventDetails = response.data;
-        return eventDetails;
-    }
+export const getEventDetailsAllFiltersExactDate = async (searchLocation, searchDateStart, searchEventType, searchType, searchTerm) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}&type=${searchEventType}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
 };
 
-export const getEventDetailsByLocation = async (searchTerm) => {
-    const response = await api.get(`events/?location=${searchTerm}`);
+export const getEventDetailsNoLocation = async (searchDateStart, searchDateEnd, searchEventType, searchType, searchTerm) => {
+    const response = await api.get(`events/?start_date=${searchDateStart}&end_date=${searchDateEnd}&type=${searchEventType}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsNoSearchTermSearchType = async (searchLocation, searchDateStart, searchDateEnd, searchEventType) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}&end_date=${searchDateEnd}&type=${searchEventType}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocExactDateSearchTermSearchType = async (searchLocation, searchDateStart, searchType, searchTerm) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocEventTypeSearchTermSearchType = async (searchLocation, searchEventType, searchType, searchTerm) => {
+    const response = await api.get(`events/?location=${searchLocation}&type=${searchEventType}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsDateRangeSearchTermSearchType = async (searchDateStart, searchDateEnd, searchType, searchTerm) => {
+    const response = await api.get(`events/?start_date=${searchDateStart}&end_date=${searchDateEnd}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocDateRange = async (searchLocation, searchDateStart, searchDateEnd) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}&end_date=${searchDateEnd}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocExactDateEventType = async (searchLocation, searchDateStart, searchEventType) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}&type=${searchEventType}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocSearchTermSearchType = async (searchLocation, searchType, searchTerm) => {
+    const response = await api.get(`events/?location=${searchLocation}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsDateRangeEventType = async (searchDateStart, searchDateEnd, searchEventType) => {
+    const response = await api.get(`events/?start_date=${searchDateStart}&end_date=${searchDateEnd}&type=${searchEventType}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsExactDateSearchTermSearchType = async (searchDateStart, searchType, searchTerm) => {
+    const response = await api.get(`events/?start_date=${searchDateStart}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsEventTypeSearchTermSearchType = async (searchEventType, searchType, searchTerm) => {
+    const response = await api.get(`events/?type=${searchEventType}&${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocExactDate = async (searchLocation, searchDateStart) => {
+    const response = await api.get(`events/?location=${searchLocation}&start_date=${searchDateStart}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocEventType = async (searchLocation, searchEventType) => {
+    const response = await api.get(`events/?location=${searchLocation}&type=${searchEventType}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsDateRange = async (searchDateStart, searchDateEnd) => {
+    const response = await api.get(`events/?start_date=${searchDateStart}&end_date=${searchDateEnd}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsExactDateEventType = async (searchDateStart, searchEventType) => {
+    const response = await api.get(`events/?start_date=${searchDateStart}&type=${searchEventType}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsSearchTermSearchType = async (searchType, searchTerm) => {
+    const response = await api.get(`events/?${searchType}=${searchTerm}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsLocation = async (searchLocation) => {
+    const response = await api.get(`events/?location=${searchLocation}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsExactDate = async (searchDateStart) => {
+    const response = await api.get(`events/?start_date=${searchDateStart}`);
+    let eventDetails = response.data;
+    return eventDetails;
+};
+
+export const getEventDetailsEventType = async (searchEventType) => {
+    const response = await api.get(`events/?type=${searchEventType}`);
     let eventDetails = response.data;
     return eventDetails;
 };
