09352666ea98d41920a33064d463ab6624eebd3a
crystaljobe <crystaljobe29@gmail.com>
2024-04-25 12:55:05 -0600

    saving progress

--- front-end/src/components/EventDetailsStaticMap.jsx
+++ front-end/src/components/EventDetailsStaticMap.jsx
@@ -9,3 +9,23 @@ mapboxgl.accessToken =
 export default function LocationSearchMap() {
 
 }
+
+//curl -g https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/pin-s+e01010(-104.774529,38.877232)/-104.7916,38.8696,10,0/300x200@2x?access_token=pk.eyJ1IjoibWNyZXlub2xkc2giLCJhIjoiY2x2MzFuNzN6MGhoOTJycnd5ZHQ3eWR4ayJ9.QKI5tsCAXhuzNb2XzhyjOg
+// const [eventCoords, setEventCoords] = useState("");
+// <Form.Group className="mb-3" controlId="event_venue_address">
+//                   <Form.Label>
+//                     Set Event Location:
+//                     <br />
+//                     <EventMap
+//                       setEventCoords={setEventCoords}
+//                       setEventVenueAddress={setEventVenueAddress}
+//                     />
+//                   </Form.Label>
+//                 </Form.Group>{" "}
+
+
+
+
+
+
+
