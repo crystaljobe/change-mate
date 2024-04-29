7f9323268bf23ac79614c0dca268e0f8990fd1bf
Kara Watts <kdwwatts@gmail.com>
2024-04-29 13:32:58 -0400

    added comments to volunteer roles serializers

--- back-end/volunteer_roles_app/serializers.py
+++ back-end/volunteer_roles_app/serializers.py
@@ -2,14 +2,16 @@ from rest_framework.serializers import ModelSerializer
 from .models import VolunteerRole
 from profile_app.serializers import UserProfileSerializer
 
-# InterestCategory categories serializer to return category 
 class CreateVolunteerRoleSerializer(ModelSerializer):
+    '''Serializer for creating a new volunteer role.'''
     class Meta:
         model = VolunteerRole
         fields = ['event', 'role', 'num_volunteers_needed']
 
 class AssignVolunteerRoleSerializer(ModelSerializer):
+    '''Serializer for put request when assigning a volunteer to a role.'''
     assigned_volunteers = UserProfileSerializer(many=True)
+
     class Meta:
         model = VolunteerRole
         fields = '__all__'
