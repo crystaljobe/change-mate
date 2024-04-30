d308497b2178b380aa7adddc18f3d3722a58b807
crystaljobe <crystaljobe29@gmail.com>
2024-04-30 13:49:11 -0600

    updated text imports and set text classes in index.cssfor consistent styling of text in app.

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,47 +1,43 @@
-import { Container, Row, Col, Card, Image } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import LlamaImg from "../assets/llama-homepage.png";
 
 export default function Homepage() {
 	return (
-		<Container style={{ marginTop: "8rem" }} className="homepage-header-text">
+		<Container fluid style={{ marginTop: "8rem" }} >
 			<Row>
-				<Col md={4} lg="true" className="text-end">
+				<Col md={4} lg={4} sm={12} className="text-end" style={{ paddingTop: "2px"}}>
 					<Image
 						src={LlamaImg}
 						rounded
-						style={{ height: "21.5rem", width: "21.5rem", marginTop: "8px" }}
+						style={{ height: "19rem", width: "19rem", marginTop: "8px", filter: "drop-shadow(gray -1rem 2rem 10px)" }}
 					/>
 				</Col>
 
-				<Col lg={true} className="text-center" style={{ paddingTop: "1.5rem" }}>
+				<Col lg={6} md={8} sm={12} className="text-center" style={{ paddingTop: "6px"}}>
 					<Row>
-						<h1
-							style={{
-								color: "#F23F8B",
-							}}>
+						<h1 className="homepage-header-text">
 							Join our vibrant platform to effortlessly organize impactful
-							events, collaborate for social change, and secure donations <br />{" "}
-							<span style={{ color: "#000000" }}>all in one place!</span>
+							events, collaborate for social change, and secure donations{" "} 
+							<span style={{ fontStyle:"italic" }}>all in one place!</span>
 						</h1>
 					</Row>
 					<br />
 					<Row className="text-center">
-						<h3
-							className="homepage-subtext"
-							style={{ color: "#ab47bc", fontStyle: "italic" }}>
+						<h4
+							className="homepage-subtext">
 							Sign up to make impactful connections today!
-						</h3>
+						</h4>
 					</Row>
 					{/* <br /> */}
 					<Row className="text-center">
-						<Col>
+						<Col className="mt-2">
 							<Button 
                                 component={Link} 
                                 to="/signup"
                                 style={{
-									marginRight: "10px",
+									marginRight: "px",
 									paddingLeft: "3rem",
 									paddingRight: "3rem",
 								}}
@@ -63,7 +59,7 @@ export default function Homepage() {
                                 component={Link} 
                                 to="/login"
                                 style={{
-									marginLeft: "10px",
+									marginLeft: "20px",
 									paddingLeft: "3.5rem",
 									paddingRight: "3.5rem",
 								}}
