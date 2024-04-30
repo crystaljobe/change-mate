830357435ed4cda63ebcf267c0d86732eeca941e
Natalie <rivero_natalie@outlook.com>
2024-04-30 10:53:52 -0400

    cleaning up admin page

--- front-end/src/pages/AdminPage.jsx
+++ front-end/src/pages/AdminPage.jsx
@@ -1,4 +1,4 @@
-import { useOutletContext, Link } from "react-router-dom";
+import { useParams, Link, useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getUserProfile } from "../utilities/UserProfileUtilities";
 import {
@@ -26,8 +26,11 @@ import { Container, Row, Col, Card, Button, CardGroup } from "react-bootstrap";
 //PUT - edit volrole {'role', 'assigned_volunteers' <userProf IDs>, 'num_volunteers_needed'}
 //DELETE - delete
 function AdminPage() {
+  // URI== /event/:eventID/admin
   const [userHostEvents, setUserHostEvents] = useState([]);
   const [eventIcon, setEventIcon] = useState("")
+  let { eventID } = useParams();
+
 
   // Use the OutletContext to get userProfileData and its setter function
   const { userProfileData, setUserProfileData } = useOutletContext(); //userProfData = {id, hosted_events}
@@ -55,57 +58,60 @@ function AdminPage() {
   console.log("ADMIN PAGE--user user_events", userHostEvents && userHostEvents);
 
   return (
-    <Container fluid >
-      <Row style={{ overflowX: "scroll", height: '40vh', overflowY: 'hidden'}}>
-        {userHostEvents && userHostEvents.length === 0 ? (
-          <h3 style={{ fontStyle: "italic" }}>
-            You are not hosting any events at this time
-          </h3>
-        ) : (
-          userHostEvents &&
-          userHostEvents.map((event) => (
-              <Card key={event.id}  style={{ width: "30vw" }}>
-                <Card.Body>
-                  <Card.Title>{event.title}</Card.Title>
-
-                  {/* Conditional rendering of event photo; If event has photo, render that; If no photo, render default event icon */}
-
-                  <Card.Img
-                    variant="top"
-                    src={(event.event_photo && event.event_photo) || eventIcon}
-                    style={{ height: "200px", width: "200px" }}
-                    alt={`${event.title}'s photo`}
-                  />
-
-                  <Card.Text>
-                    <strong> When: </strong>{" "}
-                    {` ${event.startDate} at ${event.startTime} -- ${event.endDate} at ${event.endTime}`}
-                    <br />
-                    <strong>Event Type: </strong> {event.event_type}
-                    <br />
-                    {event.event_type === "Virtual" ? null : (
-                      <>
-                        <strong>Location: </strong>
-                        {event.event_venue}
-                      </>
-                    )}
-                  </Card.Text>
-                  {/* 
-                    <Button
-                      style={{ margin: 3 }}
-                      variant="info"
-                      as={Link}
-                      to={`/editevent/${event.id}`}
-                    >
-                      Edit Event Details
-                    </Button> */}
-                </Card.Body>
-              </Card>
-          ))
-        )}
-      </Row>
-    </Container>
+   <>
+   
+   </>
   );
 }
 
 export default AdminPage;
+//  <Container fluid >
+//       <Row style={{ overflowX: "scroll", height: '40vh', overflowY: 'hidden'}}>
+//         {userHostEvents && userHostEvents.length === 0 ? (
+//           <h3 style={{ fontStyle: "italic" }}>
+//             You are not hosting any events at this time
+//           </h3>
+//         ) : (
+//           userHostEvents &&
+//           userHostEvents.map((event) => (
+//               <Card key={event.id}  style={{ width: "30vw" }}>
+//                 <Card.Body>
+//                   <Card.Title>{event.title}</Card.Title>
+
+//                   {/* Conditional rendering of event photo; If event has photo, render that; If no photo, render default event icon */}
+
+//                   <Card.Img
+//                     variant="top"
+//                     src={(event.event_photo && event.event_photo) || eventIcon}
+//                     style={{ height: "200px", width: "200px" }}
+//                     alt={`${event.title}'s photo`}
+//                   />
+
+//                   <Card.Text>
+//                     <strong> When: </strong>{" "}
+//                     {` ${event.startDate} at ${event.startTime} -- ${event.endDate} at ${event.endTime}`}
+//                     <br />
+//                     <strong>Event Type: </strong> {event.event_type}
+//                     <br />
+//                     {event.event_type === "Virtual" ? null : (
+//                       <>
+//                         <strong>Location: </strong>
+//                         {event.event_venue}
+//                       </>
+//                     )}
+//                   </Card.Text>
+//                   {/* 
+//                     <Button
+//                       style={{ margin: 3 }}
+//                       variant="info"
+//                       as={Link}
+//                       to={`/editevent/${event.id}`}
+//                     >
+//                       Edit Event Details
+//                     </Button> */}
+//                 </Card.Body>
+//               </Card>
+//           ))
+//         )}
+//       </Row>
+//     </Container>
--- front-end/src/router.jsx
+++ front-end/src/router.jsx
@@ -67,7 +67,7 @@ const router = createBrowserRouter([
         element: <EventCollab />,
       },
       {
-        path: "/admin",
+        path: "/admin", // /event/:eventID/admin
         element: <AdminPage />,
       },
     ],
