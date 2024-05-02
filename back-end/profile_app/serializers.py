2c3f59707b373c923a9655633f5d446b5db76e61
Kara Watts <kdwwatts@gmail.com>
2024-05-01 22:35:31 -0400

    added coordinates and volunteer events to profile model/ serializer

--- back-end/profile_app/serializers.py
+++ back-end/profile_app/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from .models import UserProfile
 from interest_app.serializers import InterestCategorySerializer
-from event_app.serializers import EventDetailsSerializer
+from event_app.serializers import EventCardSerializer
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    user_events = EventDetailsSerializer(many=True)
-    events_attending = EventDetailsSerializer(many=True)
+    user_events = EventCardSerializer(many=True)
+    events_attending = EventCardSerializer(many=True)
     interests = InterestCategorySerializer(many=True)
 
     class Meta: 
         model = UserProfile
-        fields = ['id', 'display_name', 'location', 'image', 'interests', 'user_events', 'events_attending']
+        fields = ['id', 'display_name', 'location', 'image', 'interests', 'user_events', 'events_attending', 'volunteer_events']
 
 class DisplayNameSerializer(serializers.ModelSerializer):
     class Meta:
